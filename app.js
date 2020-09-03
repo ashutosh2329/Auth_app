3ad02a8f765113b8bf2977a3ff4042b7298fd440
ashutosh2329 <sonutiwari2329@gmail.com>
2020-09-04 02:50:04 +0530

    changes

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//jshint esversion:6
+// required the neccesary packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const load = require('lodash');
+const mongoose = require("mongoose");
+const app = express();
+
+
+
+// ejs setting
+app.set('view engine', 'ejs');
+
+
+// bodyparser use and ejs template 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+	res.render("home");
+})
+
+
+
+// server running port
+app.listen(8000, function() {
+  console.log("Server started on port 8000");
+});
+
