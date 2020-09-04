2c472df2e202306135ca4ba3ef7c61af04fe28f6
ashutosh2329 <sonutiwari2329@gmail.com>
2020-09-04 21:27:08 +0530

    level 3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const load = require('lodash');
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 
 
@@ -27,8 +27,8 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-// encryption
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+
 // user model
 const User = mongoose.model('User', userSchema);
 
@@ -56,7 +56,7 @@ app.get("/login",function(req,res){
 app.post("/register",function(req,res){
 	const newUser = new User({
 		email: req.body.username,
-		password: req.body.password
+		password: md5(req.body.password)
 	});
 	newUser.save(function(err){
 		if(err){
@@ -71,7 +71,7 @@ app.post("/register",function(req,res){
 // post route for login
 app.post("/login",function(req,res){
 	const email = req.body.username;
-	const passkey = req.body.password;
+	const passkey = md5(req.body.password);
 	User.findOne({email:email},function(err,founduser){
 		if(err){
 			console.log(err);
