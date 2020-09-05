5ef91e98c1749e604b7f6685b21a8fa16a68ef07
ashutosh2329 <sonutiwari2329@gmail.com>
2020-09-05 23:49:34 +0530

    changes

--- app.js
+++ app.js
@@ -7,32 +7,50 @@ const load = require('lodash');
 const mongoose = require("mongoose");
 const md5 = require('md5');
 const app = express();
+
+
 // mongodb connection
 mongoose.connect('mongodb://localhost:27017/AuthDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+
 // ejs setting
 app.set('view engine', 'ejs');
+
+
 // bodyparser use and ejs template 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
 // user schema 
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
+
+
 // user model
 const User = mongoose.model('User', userSchema);
+
+
 //home route
 app.get("/",function(req,res){
 	res.render("home");
 });
+
+
 //register route
 app.get("/register",function(req,res){
 	res.render("register");
 });
+
+
 // login route
 app.get("/login",function(req,res){
 	res.render("login");
-})
+});
+
+
 // post route for register
 app.post("/register",function(req,res){
 	const newUser = new User({
@@ -46,7 +64,7 @@ app.post("/register",function(req,res){
 			res.render("login");
 		}
 	});
-})
+});
 
 
 // post route for login
@@ -68,7 +86,7 @@ app.post("/login",function(req,res){
 			}
 		}
 	}); 
-})
+});
 
 
 // server running port
