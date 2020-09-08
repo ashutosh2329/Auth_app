608969bd84d99b3ddf68d0745b7a2c4c3c992fe4
ashutosh2329 <sonutiwari2329@gmail.com>
2020-09-08 22:58:16 +0530

    changes

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/AuthDB', {useNewUrlParser: true, useUnifiedTopology: true });
 
 
+
+
 // ejs setting
 app.set('view engine', 'ejs');
+
+
 
 
 // bodyparser use and ejs template 
